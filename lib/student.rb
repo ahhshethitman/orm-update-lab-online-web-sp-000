d2453227c1a6d66a6680aff70c8c2b2caceecd3b
Hunter Sides <hunter.s.1337@gmail.com>
2020-08-13 22:45:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class	  
   def initialize(id=nil, name, grade)
-  #  with DB[:conn]  	    @id, @name, @grade = id, name, grade
+   	  @id, @name, @grade = id, name, grade
 
   end
 
